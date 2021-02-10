ff56b910a1a6e181ae3bfc0ca974637c3da60d3f
niroshan.nadarajah <niroshan.nadarajah@gamesys.co.uk>
2021-02-10 10:11:13 +0000

    lect: 34 modify django admin to support creating users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -59,3 +59,10 @@ class AdminSiteTests(TestCase):
         # 3.2 Test failing because setUp user doesn't have last_name field
         # self.assertContains(res, self.user.last_name)
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
